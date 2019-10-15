0c4f4b4b8754b9fa40dc40ae494948284881fa54
pogonwnm <56569912+pogonwnm@users.noreply.github.com>
2019-10-14 20:39:13 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,6 @@ import discord
 import time
 import asyncio
 
-
-def read_token():
-    with open("token.txt", "r") as f:
-        lines = f.readlines()
-        return lines[0].strip()
-
-token = read_token()
-
 client = discord.Client()
 
 @client.event
